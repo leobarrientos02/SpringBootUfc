b2720c65b1c9abe82ca40965e1ddfc07d1e3f5ec
leobarrientos02 <leobarrientos02@gmail.com>
2022-08-01 13:41:23 -0400

    testing

--- src/test/java/com/example/ufc/UfcApplicationTests.java
+++ src/test/java/com/example/ufc/UfcApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.ufc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@SpringBootTest
 class UfcApplicationTests {
 
 	Calculator underTest = new Calculator();
